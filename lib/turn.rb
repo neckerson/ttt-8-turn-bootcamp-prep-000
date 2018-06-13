285cc455d39b900cb610c80ad5f4ad87acb96430
Nate Eckerson <nate.eckerson@gmail.com>
2018-06-13 18:59:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,25 @@ def position_taken?(board, index)
     true
   end
 end
+
+def display_board(board, board_array = [])
+  
+  e = 0
+  
+  3.times do |i|
+  
+  board_array << " #{board[e]} | #{board[e+1]} | #{board[e+2]} "  
+    
+    if i/2 == 0
+      board_array << "-----------\n"
+    end
+  
+  e += 3  
+  
+  end
+  
+  board_array.each do |x|
+    puts x
+  end
+
+end
