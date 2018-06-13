16653f320fe9bb06f340cfa05990e38ef8f6acfa
Nate Eckerson <nate.eckerson@gmail.com>
2018-06-13 18:55:19 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if index > board.length 
-    true
+    false
   else
-    position_taken?(board, index)
+    !position_taken?(board, index)
   end
 end
 
