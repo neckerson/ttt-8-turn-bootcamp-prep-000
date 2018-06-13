f2aeac0f9243688b383ecb17d176cb2720845892
Nate Eckerson <nate.eckerson@gmail.com>
2018-06-13 19:23:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -47,7 +47,7 @@ def turn(board)
   puts 'Please enter 1-9:'
   input = gets.strip
   if valid_move? board, input_to_index(input)
-    move(board, input)
+    move(board, input_to_index(input))
   else
     turn(board)
   end
