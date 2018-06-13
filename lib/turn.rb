fdbe09eaa913b4dfdf6165648a26c255474aaee9
Nate Eckerson <nate.eckerson@gmail.com>
2018-06-13 19:00:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,24 +17,26 @@ def position_taken?(board, index)
   end
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board_array, board_index, character = "X")
+  board_array[board_index] = character
+end
+
 def display_board(board, board_array = [])
-  
   e = 0
-  
   3.times do |i|
   
-  board_array << " #{board[e]} | #{board[e+1]} | #{board[e+2]} "  
-    
+  board_array << " #{board[e]} | #{board[e+1]} | #{board[e+2]} "
     if i/2 == 0
       board_array << "-----------\n"
     end
-  
   e += 3  
-  
   end
   
   board_array.each do |x|
     puts x
   end
-
 end
